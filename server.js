d3bcef111838dfb353278f5a438f1eabade396c8
Alexey Sikora <tolstosheev00@mail.ru>
2022-06-26 06:18:45 +0400

    first commit

--- server.js
+++ server.js
@@ -1,15 +1,9 @@
-//Install express server
 const express = require('express');
 const path = require('path');
-
 const app = express();
+app.use(express.static(__dirname + '/dist/sonya'));
+app.get('/*', function(req,res)
 
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/sonya'));
+{res.sendFile(path.join(__dirname+'/dist/sonya/index.html'));});
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/sonya/'}),
-);
-
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
