4292d6fc7c48e2b9f377c8e842bf2e26548de08b
Alexey Sikora <tolstosheev00@mail.ru>
2022-06-27 02:17:59 +0400

    new

--- src/app/main-screen/main-screen.component.ts
+++ src/app/main-screen/main-screen.component.ts
@@ -4,6 +4,7 @@ import { CommandRecognitionService } from '../service/command-recognition.servic
 import { VoiceRecognitionService } from '../service/voice-recognition.service';
 import {TEMP} from '../service/extendedData.js'
 import tts from 'tts-js'
+import { startVR, stopVR, getTextVR} from '../service/vr.js'
 
 
 @Component({
@@ -33,16 +34,12 @@ export class MainScreenComponent implements OnInit {
     if (this.recording == true)
     {
       this.mainText = '...'
-      this.srService.start()
+      startVR()
     }
     else
     {
-      this.srService.stop()
-      this.mainText = ''
-      if(typeof this.srService.text !== 'undefined')
-        this.mainText = this.srService.text
-      else
-        this.mainText = ''
+      stopVR()
+      this.mainText = getTextVR()
 
       await new Promise(f => setTimeout(f, 2000)); 
       var commandResult = this.crService.getCommand(this.mainText);
